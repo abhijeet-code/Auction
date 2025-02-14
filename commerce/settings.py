71f0ad8dd3fdf062d4a1136f9f30017bd295d9cb
Abhi <mymail12@gmail.com>
2025-02-14 23:15:28 +0530

    css

--- commerce/settings.py
+++ commerce/settings.py
@@ -74,24 +74,13 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentDB',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '/tmp/db.sqlite3'),
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, '/tmp/db.sqlite3'),
-#     }
-# }
 
 AUTH_USER_MODEL = 'auctions.User'
 
